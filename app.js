526c506cffa11cf9ed10aaf3002cd5eb11581f6a
mrjuanjooc <juanjoseocampo95@gmail.com>
2020-02-25 22:56:15 -0500

    Creando Proyecto

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  config = require("./app/configs/config.js"),
+  router = express.Router(),
+  routes = require("./app/routes/index.js"),
+  mongoose = require("mongoose");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use(bodyParser.json());
+
+routes(router);
+app.use("/api", router);
+
+mongoose.connect(
+  "mongodb://" + config.BD_IP + ":" + config.BD_PORT + "/" + config.BD_NAME,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+  },
+  err => {
+    if (err) {
+      console.log("Fallas al conectar, error: " + err);
+    }
+  }
+);
+
+let db = mongoose.Connection;
+db.on("error", console.error.bind(console, "Error Conexión Base de Datos"));
+
+if (!db) {
+  console.log("Error Durante la conexión con la Base de Datos");
+} else {
+  console.log("Conexión Exitosa a la Base de Datos");
+}
+
+app.listen(config.PORT, () => {
+  console.log("Escuchando el puerto: " + config.PORT);
+});
