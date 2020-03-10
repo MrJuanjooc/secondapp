45c8519cd8e0351cbfd784162ba7f24fdcb5d85e
mrjuanjooc <juanjoseocampo95@gmail.com>
2020-03-09 20:05:16 -0500

    Cambio authController y authService

--- app/services/authService.js
+++ app/services/authService.js
@@ -1,14 +1,25 @@
 const jwt = require("jsonwebtoken");
 const config = require("../configs/config");
 const userService = require("../services/userService");
+const userModel = require("../models/userModel");
 
-exports.autenticar = (user, pass) => {
-  if (user == pass) {
-    const token = jwt.sign({ id: user }, config.SECRET, { expiresIn: 10000 });
-    return token;
-  } else {
-    return "Error";
+exports.autenticar = async (username, pass) => {
+  const user = await userService.getUserByUsername(username); //se busca el usuario por el nombre de usuario
+  if (!user) {
+    throw new Error("Usuario no encontrado o no Existe");
   }
+
+  const Result = await user.comparePassword(pass);
+  console.log(Result);
+
+  if (!Result) {
+    throw new Error("Password Invalido");
+  }
+  const token = jwt.sign({ user: user._id }, config.SECRET, {
+    expiresIn: 10000
+  }); //retorno el token por el id del usuario
+
+  return token;
 };
 
 exports.signUp = async user => {
